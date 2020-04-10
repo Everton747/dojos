c0865bc6fefc05a393ce7aea9d33202485815fb8
Everton747 <everton_747@hotmail.com>
2020-04-09 23:16:16 -0300

    lint fix

--- src/Jokenpo.js
+++ src/Jokenpo.js
@@ -1,24 +1,25 @@
 const Jokenpo = (playA, playB) => {
+  if (!['rock', 'paper', 'scissors'].includes(playA)) return 'invalid choice';
+  if (!['rock', 'paper', 'scissors'].includes(playB)) return 'invalid choice';
 
-  if(!['rock', 'paper', 'scissors'].includes(playA)) return 'invalid choice';
-  if(!['rock', 'paper', 'scissors'].includes(playB)) return 'invalid choice';
+  if (playA === playB) return 'draw';
 
-  if(playA === playB) return 'draw';
-
-  if(playA === 'rock') {
-    if(playB === 'scissors') return 'rock';
-    if(playB === 'paper') return 'paper';
+  if (playA === 'rock') {
+    if (playB === 'scissors') return 'rock';
+    if (playB === 'paper') return 'paper';
   }
 
-  if(playA === 'scissors') {
-    if(playB === 'paper') return 'scissors';
-    if(playB === 'rock') return 'rock';
+  if (playA === 'scissors') {
+    if (playB === 'paper') return 'scissors';
+    if (playB === 'rock') return 'rock';
   }
 
-  if(playA === 'paper') {
-    if(playB === 'rock') return 'paper';
-    if(playB === 'scissors') return 'scissors';
+  if (playA === 'paper') {
+    if (playB === 'rock') return 'paper';
+    if (playB === 'scissors') return 'scissors';
   }
+
+  return undefined;
 };
 
 export default Jokenpo;
